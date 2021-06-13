9c111d28dcc112265b777876a3d43855f9e4f477
Noncenze <thmas.dee@gmail.com>
2021-06-13 02:32:36 -0400

    Add styling to the NavBar

--- pantry-app/src/components/SearchBar.js
+++ pantry-app/src/components/SearchBar.js
@@ -37,17 +37,23 @@ const SearchBar = (props) => {
   };
 
   return (
-    <div>
+    <div className="search">
       <form onSubmit={submitForm}>
-        <input type="text" name="name" value={search.value} onChange={handleInput}/>
-        <select>
+        <select className="selector-btn">
           <option name="firstLetter">All</option>
           <option name="name">Recipe</option>
           <option name="filterIngredient">Ingredient</option>
           <option name="filterCategory">Categories</option>
           <option name="filterArea">Area</option>
         </select>
-        <button type="submit" className="btn btn-secondary"> Search </button>
+        <input type="text" name="name" value={search.value} onChange={handleInput} className="search-input" placeholder="Enter a recipe..."/>
+        <button type="submit" className="search-btn">
+          {/* Magnifying Glass Icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+          {/* --- Icon End --- */}
+        </button>
       </form>
     </div>
   );
